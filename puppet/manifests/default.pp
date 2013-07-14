8624e62bb8ed72aef520bf5b12c50864d3cc1a6e
Brenno Costa <brennolncosta@gmail.com>
2013-07-14 19:17:32 -0300

    Split postgres recipes from default

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -8,21 +8,3 @@ class update_packages {
   }
 }
 class { 'update_packages': }
-
-class { 'postgresql':
-  charset => 'UTF8',
-  locale => 'en_US.UTF-8',
-  manage_package_repo => true,
-  version => '9.2',
-
-  pg_user { 'vagrant':
-    ensure => present,
-    superuser => true,
-    password => 'vagrant',
-    require => Class['postgresql::server']
-  },
-
-  package { 'libpq-dev':
-    ensure => installed
-  }
-}
