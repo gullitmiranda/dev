74657e70508918cdba4f93e6d80e0f221390847c
Brenno Costa <brennolncosta@gmail.com>
2013-09-15 22:57:18 -0300

    Add default packages

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -2,8 +2,12 @@ include_recipe 'apt'
 include_recipe 'openssl'
 include_recipe 'set_locale'
 
+# Ensure locales
+ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = 'en_US.UTF-8'
+
+# Postgres configuration
 node.set['postgresql']['password']['postgres'] = 'password'
-node['postgresql']['pg_hba'] = [
+node.set['postgresql']['pg_hba'] = [
   {
     :type => 'local',
     :db => 'all',
@@ -12,5 +16,15 @@ node['postgresql']['pg_hba'] = [
   }
 ]
 
-ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = 'en_US.UTF-8'
 include_recipe 'postgresql::server'
+
+# Packages
+%w(
+  build-essential git-core subversion curl autoconf zlib1g-dev libssl-dev
+  libreadline6-dev libxml2-dev libyaml-dev libapreq2-dev
+  imagemagick libmagickwand-dev libxslt1-dev libxml2-dev sphinxsearch
+).each do |package_name|
+  package package_name do
+    action :install
+  end
+end
