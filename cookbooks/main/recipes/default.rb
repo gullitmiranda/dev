ea641fee3bfc0b3d0978887d85782a21b95e5477
Brenno Costa <brennolncosta@gmail.com>
2013-09-15 22:33:31 -0300

    Add a set locale config to enforce and setup en_US-UTF8

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,14 +1,16 @@
 include_recipe 'apt'
 include_recipe 'openssl'
+include_recipe 'set_locale'
 
-node.set['postgresql']['password']['postgres'] = 'postgres'
-node.set['postgresql']['pg_hba'] = [
+node.set['postgresql']['password']['postgres'] = 'password'
+node['postgresql']['pg_hba'] = [
   {
     :type => 'local',
     :db => 'all',
-    :user => 'all',
+    :user => 'postgres',
     :method => 'trust'
   }
 ]
 
+ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = 'en_US.UTF-8'
 include_recipe 'postgresql::server'
