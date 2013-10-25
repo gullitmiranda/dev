5b05fde328f354e2d1755864efb4925bb099bad0
Fabio Akita <akitaonrails@Hal9001c.local>
2013-10-25 15:52:32 -0300

    Changing to YADR dotfiles.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -64,21 +64,7 @@ end
 # Dotfiles
 bash 'clone dotfiles repo' do
   user 'vagrant'
-  cwd '/home/vagrant/code'
-  code 'git clone https://github.com/brennovich/dotfiles.git /home/vagrant/code/dotfiles'
-  not_if { ::File.exists?('/home/vagrant/code') }
-end
-
-bash 'install dotfiles' do
-  user 'vagrant'
-  cwd '/home/vagrant/code/dotfiles'
-  code 'rm /home/vagrant/.bashrc && HOME=/home/vagrant sh /home/vagrant/code/dotfiles/install.sh'
-  not_if { ::File.exists?('/home/vagrant/.vimrc') }
-end
-
-bash 'setup neobundle' do
-  user 'vagrant'
-  cwd '/home/vagrant/'
-  code 'git clone git://github.com/Shougo/neobundle.vim /home/vagrant/.vim/bundle/neobundle.vim'
-  not_if { ::File.exists?('/home/vagrant/.vim/bundle/neobundle.vim') }
+  cwd '/home/vagrant'
+  code 'sh -c "`curl -fsSL https://raw.github.com/skwp/dotfiles/master/install.sh`"'
+  not_if { ::File.exists?('/home/vagrant/.yadr') }
 end
