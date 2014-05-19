4d7cd04edec5ffdd152e1e4ab72b186121d5dd24
Gullit Miranda <gullitmiranda@requestdev.com.br>
2014-05-19 11:45:49 -0300

    update git modules and remove mysql, redis, elastic search, mongodb

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -17,44 +17,22 @@ node.set['apache'] = {
 }
 
 # Postgres
-node.set['postgresql'] = {
-  :version => "9.3",
-  :contrib => {
-    :packages => "postgresql-contrib-9.3",
-    :extensions => ['hstore']
-  },
-  :password => {
-    :postgres => ''
-  },
-  :pg_hba => [
-    {
-      :type => 'local',
-      :db => 'all',
-      :user => 'postgres',
-      :method => 'trust'
-    }
-  ]
-}
-
-# Mysql
-node.set['mysql'] = {
-  :server_root_password => '',
-  :server_repl_password => '',
-  :server_debian_password => '',
-  :allow_remote_root => true,
-  :bind_address => '*',
-
-  :client => {
-    :packages => ['libmysqlclient-dev php5-imagick php5-curl']
-  }
-}
+node['postgresql'][:version] = "9.3"
+node['postgresql'][:contrib][:packages] = "postgresql-contrib-9.3"
+node['postgresql'][:contrib][:extensions] = ['hstore']
+node['postgresql'][:password][:postgres] = 'postgres'
+node['postgresql'][:enable_pgdg_apt] = true
+node['postgresql'][:pg_hba] = [{:type => 'local',
+                                :db => 'all',
+                                :user => 'postgres',
+                                :method => 'trust'}]
 
 # rvm
 node.set['rvm']['installer_url'] = "https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer"
 node.set['rvm']['user_installs'] = [ {
   :user => 'vagrant',
   :install_rubies => true,
-  :default_ruby => 'ruby-2.0.0-p353'
+  :default_ruby => 'ruby-2.1'
 } ]
 node.set['rvm']['vagrant'] = {
   :system_chef_solo => '/usr/local/bin/chef-solo'
@@ -62,14 +40,6 @@ node.set['rvm']['vagrant'] = {
 
 # Heroku Toolbelt
 node.set['heroku-toolbelt']['standalone'] = false
-
-# Mongo DB
-node.set[:mongodb] = {
-  :version => "2.4.0"
-}
-
-# RedisIO
-node.set['redis']['source']['version'] = "2.8.2"
 
 # Python
 node.set["python"]["install_method"] = "package"
@@ -80,13 +50,7 @@ include_recipe 'build-essential'
 include_recipe 'apache2'
 include_recipe 'apache2::mod_php5'
 include_recipe 'apache2::mod_rewrite'
-include_recipe 'elasticsearch'
 include_recipe 'postgresql::server'
-include_recipe 'mysql::server'
-include_recipe 'mysql::client'
-include_recipe 'redis'
-include_recipe 'mongodb::10gen_repo'
-include_recipe 'mongodb::default'
 include_recipe 'python'
 include_recipe 'python::pip'
 include_recipe 'python::virtualenv'
@@ -96,7 +60,7 @@ include_recipe 'heroku-toolbelt'
 include_recipe 'phantomjs::default'
 
 # Packages
-%w(build-essential curl vim git-core git-flow htop imagemagick ruby1.9.3).each do |package_name|
+%w(build-essential curl vim git-core git-flow htop imagemagick).each do |package_name|
   package package_name do
     action :install
   end
